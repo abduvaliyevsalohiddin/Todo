5d9c378b6ad34888acbdb22ceda7d679b218224a
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-10-29 22:43:54 -0700

    imtihon 3

--- asosiy/views.py
+++ asosiy/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def home(requets):
+    return render(requets,"home.html")
+
+
+
+
+def hamma_studentlar(request):
+    content = {
+        "studentlar": Student.objects.all()
+    }
+    return render(request, "hamma_studentlar.html", content)
