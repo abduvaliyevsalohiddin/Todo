38a27cd3dd27410d21e27f5eb250deea58a12dec
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-10-29 23:51:30 -0700

    imtihon 12

--- asosiy/admin.py
+++ asosiy/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
-admin.site.register(Student)
-admin.site.register(Malumot)
+
+
+@admin.register(Malumot)
+class MalumotAdmin(admin.ModelAdmin):
+    list_display = ["id", "sarlavha", "sana", "malumot", "bajatilgan", "student"]
+    list_filter = ["bajatilgan"]
+    search_fields = ["sarlavha"]
+    search_help_text = "Sarlavha ustunlari bo'yicha qidiring"
+    list_display_links = ["sarlavha"]
+    autocomplete_fields = ["student"]
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["id", "ism", "yosh", "kurs", "student_raqam"]
+    list_filter = ["kurs"]
+    search_fields = ["ism"]
+    search_help_text = "Ism ustunlari bo'yicha qidiring"
+    list_display_links = ["ism"]
+
+# admin.site.register(Student)
+# admin.site.register(Malumot)
